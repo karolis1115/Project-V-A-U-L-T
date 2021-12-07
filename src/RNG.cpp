06b4b8bbd08f122797020e3bbb3401da70780774
Karolis <karolisjuoz@gmail.com>
2021-12-07 15:32:15 +0100

    Slight code cleanup in RNG.cpp

--- src/RNG.cpp
+++ src/RNG.cpp
@@ -3,7 +3,9 @@
 int generateRandomNumber(int min, int max, int &num1, int &num2, int &num3, int &num4)
 {
   randomSeed(analogRead(A5)); // generates a seed from the given value that is used in the random() function
-  num1 = random(min, max);    // generates a random number between min and max (for 1 digit it's from 0 to 9).
+
+  // generates a random number between min and max (for 1 digit it's from 0 to 9).
+  num1 = random(min, max);
   num2 = random(min, max);
   num3 = random(min, max);
   num4 = random(min, max);
