38f2eefcbc84cb8a7322fb6d0c7aad52c8e35fe4
Karolis <karolisjuoz@gmail.com>
2021-12-08 23:52:14 +0100

    Added files and basic test functions for servo and speaker

---
+++ src/Buzzer.cpp
@@ -0,0 +1,5 @@
+#include "global.h"
+//A quick buzzer test, the frequency will change based on the rotary encoder count
+void rotaryBuzz(int val){
+    tone(speaker, map(val,0,9,1000,10000));
+}
---
+++ src/Servo.cpp
@@ -0,0 +1,9 @@
+
+#include "global.h"
+
+Servo lock;
+//A qiuick servo test that takes reads a number form 0 to 9 from the rotary encoder and maps it to the degrees the servo can turn 
+void rotaryServo(int val)
+{
+        lock.write(map(val,0,9,0,180));
+}
